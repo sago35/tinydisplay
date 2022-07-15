e219b1185073d01552b7bd1b051fe4487424bf2d
sago35 <sago35@gmail.com>
2022-07-15 13:48:57 +0900

    Update ./examples/initdisplay/wioterminal.go

--- examples/initdisplay/wioterminal.go
+++ examples/initdisplay/wioterminal.go
@@ -16,7 +16,7 @@ func InitDisplay() *ili9341.Device {
 		SCK:       machine.LCD_SCK_PIN,
 		SDO:       machine.LCD_SDO_PIN,
 		SDI:       machine.LCD_SDI_PIN,
-		Frequency: 40000000,
+		Frequency: 60000000,
 	})
 
 	d := ili9341.NewSPI(
@@ -25,13 +25,13 @@ func InitDisplay() *ili9341.Device {
 		machine.LCD_SS_PIN,
 		machine.LCD_RESET,
 	)
-	d.Configure(ili9341.Config{})
+	d.Configure(ili9341.Config{
+		Rotation: ili9341.Rotation270,
+	})
 	d.FillScreen(color.RGBA{255, 255, 255, 255})
 
 	machine.LCD_BACKLIGHT.Configure(machine.PinConfig{machine.PinOutput})
 	machine.LCD_BACKLIGHT.High()
-
-	d.SetRotation(ili9341.Rotation270)
 
 	return d
 }
